90e3b84cb112229e3623c0e616bfea31fd7034cb
lyt <ldj_yt@outlook.com>
2021-01-06 09:36:57 +0800

    test

--- tour-page.js
+++ tour-page.js
@@ -1,12 +1,12 @@
 
-var fs = require('fs');
-var http = require('https');
-var cheerio = require('cheerio');
+let fs = require('fs');
+let http = require('https');
+let cheerio = require('cheerio');
 
-var transferPath = [
-    // { path: '/tour/xiamentour/xm-1/', code: 'xm-1' },
+let transferPath = [
+    { path: '/tour/xiamentour/xm-1/', code: 'xm-1' },
     // { path: '/tour/lijiangtour/lj-1/', code: 'lj-1' },
-    { path: '/tour/suzhoutour/sz-1a/', code: 'sz-1a' },
+    // { path: '/tour/suzhoutour/sz-1a/', code: 'sz-1a' },
 ]
 transferPath.forEach(function (ele, i) {
     loadPage(ele.path, ele.code).then(function (htmlJSON) {
@@ -15,21 +15,22 @@ transferPath.forEach(function (ele, i) {
 })
 
 function loadPage(path, code="") {
-    var pm = new Promise(function (resolve, reject) {
-        var options = {
+    let pm = new Promise(function (resolve, reject) {
+        let options = {
             host: 'www.chinahighlights.com',
             path: path
             // port: 80,
         };
-        var html = '';
+        let html = '';
         http.get(options, function (res) {
             res.on('data', function (data) {
                 // collect the data chunks to the variable named "html"
                 html += data;
             }).on('end', function () {
-                var $ = cheerio.load(html);
-                var htmlData = {
+                let $ = cheerio.load(html);
+                let htmlData = {
                     tourCode: code,
+                    url: path,
                     description: $('meta[name="description"]').attr('content'),
                     keywords: $('meta[name="keywords"]').attr('content'),
                     topImg: $('.TopCht1 img.visible-xs').attr('src'),
@@ -40,9 +41,14 @@ function loadPage(path, code="") {
                     highlights: $('.highlights ul').html(),
                     itinerary: []
                 }
-                // todo: Dinner; infoimage,infoimagetitle; form
+                // todo: Dinner; tourimg,imgname;
                 $('.touritinerary .TourList').each(function (i, tourlist) {
-                    var tourDay = {
+                    $(tourlist).children('.TourInfo').children().each(function (j, p){
+                        if ($(p).find('img')) {
+                            $(p).replaceWith(`<div class="tourimg"><img alt="Enjoy the beautiful lake view in the Summer Palace" class="TopImage img-responsive" src="https://data.chinahighlights.com/image/tour-detail/amp-image/bj-5-day-4-1.jpg"> <span class="imgname">Enjoy the beautiful lake view in the Summer Palace</span></div>`);
+                        }
+                    });
+                    let tourDay = {
                         day: $(tourlist).children('.TourDates').children('.TourDay').text(),
                         title: $(tourlist).children('.TourDates').text(),
                         TourInfo: $(tourlist).children('.TourInfo').html()
@@ -50,7 +56,7 @@ function loadPage(path, code="") {
                     tourDay.title = tourDay.title.replace(tourDay.day, '');
                     htmlData.itinerary.push(tourDay);
                 });
-                var lastInfo = [];
+                let lastInfo = [];
                 ($('.touritinerary .TourList').parent().children().each(function (i, el) {
                     if (!$(el).hasClass('TourList')) lastInfo.push($(el).prop('outerHTML'));
                 }))
@@ -67,7 +73,7 @@ function loadPage(path, code="") {
 }
 
 function tourTemplate(htmlJson) {
-    var tourdetail = htmlJson.itinerary.map(tourD => {
+    let tourdetail = htmlJson.itinerary.map(tourD => {
         return `
 <div class="tourbg">
   <div class="tourinfo"><span class="tourdate">${tourD.day}</span> <span class="toursite">${tourD.title}</span> ${tourD.TourInfo}
@@ -77,7 +83,7 @@ function tourTemplate(htmlJson) {
 </div>
         `;
     }).join('');
-    var htmlStr = `
+    let htmlStr = `
 <!--description
 ${htmlJson.description}
 -->
@@ -128,15 +134,31 @@ ${htmlJson.keywords}
   ${htmlJson.last}
 </div>
 
-<div class="footerBtn">
-  <a href="/secureforms/inquiry?cli_no=${htmlJson.tourCode}" rel="nofollow" target="_top"><button type="submit">Inquiry Now<i aria-hidden="true" class="fa fa-angle-right"></i></button></a>
+<div class="inquirybutton"><a href="#iqnuirybutton">Inquire <img alt="" class="img-responsive" height="10px" src="//data.chinahighlights.com/pic/amp-inquiry-button-arrow.png" width="16px"> </a></div>
+<div class="tmbottom">
+<p>INQUIRE ABOUT THIS TOUR</p>
+<a id="iqnuirybutton"></a>
+
+<form action="https://www.chinahighlights.com/secureforms/qi_save" id="quick_inquiry_form" method="post" name="quick_inquiry_form" novalidate="" onsubmit="return validateQuickInquiryForm()">
+<div class="InquiryBox">
+<p><input class="FullName" id="realname" name="realname" placeholder="Full name" type="text"> <span id="realname_errmsg" style="display: none"><span class="requiredArea">Please enter your full name</span></span></p>
+
+<p><input class="EmailAddress" id="email" name="email" placeholder="Email" type="text"> <span id="email_errmsg" style="display: none"><span class="requiredArea">Please enter your email</span></span></p>
+
+<p class="InquiryDate"><input class="InquiryCalendar flatpickr-input" data-min-date="7" id="starting_date" name="starting_date" placeholder="Starting date" readonly="readonly" type="text"><span id="starting_date_errmsg" style="display: none"><span class="requiredArea">Please verify your email</span></span></p>
+
+<p><input class="Inquiryphone" id="PhoneNo" name="PhoneNo" placeholder="Any other quick ways to reach you..." style="padding-left: 50px;" type="tel"></p>
+
+<p><t_e_x_t_a_r_e_a id="form_additionalrequirements" name="form_additionalrequirements" placeholder="Tell us your tour ideas: where to visit, how many people and days, and your hotel style..."></t_e_x_t_a_r_e_a></p>
+<t_e_x_t_a_r_e_a name="nullemail" style="display: none"></t_e_x_t_a_r_e_a> <input name="cli_no" type="hidden" value="${htmlJson.tourCode}"> <input id="url" name="url" type="hidden" value="https://www.chinahighlights.com${htmlJson.url}"><button class="sendButton" id="quick_inquiry_button" name="quick_inquiry_button" type="submit">Send My Inquiry <i aria-hidden="true" class="fa fa-angle-right"></i></button></div>
+</form>
 </div>
     `;
     return htmlStr;
 }
 
 function writeFile(htmlStr, target_file_name = 'tmp') {
-    target_file_name = target_file_name.replace(/^\/?|\/?$/g, '').replace(/\/+/g, '.') + '.html';
+    target_file_name = target_file_name.replace(/^\/?|\/?$/g, '').replace(/\/+/g, '.') + '.2.html';
     fs.writeFileSync(target_file_name, htmlStr, function (err) {
         if (err) {
             console.log("write " + err.message)
